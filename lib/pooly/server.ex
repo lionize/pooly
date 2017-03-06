738fd811ca73b679e3d51d1b5aa0c5e9be13a797
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-06 14:34:59 +0000

    Add consumer crash checking to Server

--- lib/pooly/server.ex
+++ lib/pooly/server.ex
@@ -86,6 +86,18 @@ defmodule Pooly.Server do
     {:noreply, %{state | worker_sup: worker_sup, workers: workers}}
   end
 
+  def handle_info({:DOWN, ref, _, _, _}, state = %{monitors: monitors, %workers: workers}) do
+    case :ets.match(monitors, {:"$1", ref}) do
+      [[pid]] ->
+        true = :ets.delete(monitors, pid)
+        new_state = %{state | workers: [pid|workers]}
+        {:noreply, new_state}
+
+      [[]] ->
+        {:noreply, state}
+    end
+  end
+
   #
   # Helper Functions
   #
