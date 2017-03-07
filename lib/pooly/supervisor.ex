fde5fe5004976ba677035f4a02bfa054b6c306a7
Mark Chandler <mark.allen.chandler@gmail.com>
2017-03-06 21:26:58 -0500

    Change Supervisor to supervise Server and PoolsSupervisor

--- lib/pooly/supervisor.ex
+++ lib/pooly/supervisor.ex
@@ -1,13 +1,15 @@
 defmodule Pooly.Supervisor do
   use Supervisor
 
-  def start_link(pool_config) do
-    Supervisor.start_link(__MODULE__, pool_config)
+  def start_link(pools_config) do
+    Supervisor.start_link(__MODULE__, pools_config,
+                          name: __MODULE__)
   end
 
-  def init(pool_config) do
+  def init(pools_config) do
     children = [
-      worker(Pooly.Server, [self, pool_config])
+      supervisor(Pooly.PoolsSupervisor, []),
+      worker(Pooly.Server, [pool_config])
     ]
 
     opts = [strategy: :one_for_all]
